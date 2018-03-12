05e519c9eac54a60dd38a233e484e97d64e24c92
Ikey Benzaken <ikey.benz@gmail.com>
2018-03-12 03:18:56 -0400

    Linked up google form

--- DeliveryPage/app.js
+++ DeliveryPage/app.js
@@ -1,6 +1,15 @@
-function printDetails() {
-    const fields = document.getElementsByClassName('formFields');
-    for (i in fields) {
-        console.log(fields[i].value);
-    }
+function submitInfo() {
+    $.ajax({
+      url: "https://docs.google.com/forms/d/e/1FAIpQLSc4Nr9DqNBjYJ7VJ2FJsEdBkPFPpA-DZZsT3aUkrXN_hCSaGA/formResponse",
+      data: {"entry.617376864":document.getElementById('name').value,
+             "entry.760495950":document.getElementById('phone').value,
+             "entry.69628245":document.getElementById('building').value,
+             "entry.177428637":document.getElementById('floor').value,
+             "entry.1684890302":document.getElementById('room').value,
+             "entry.825117565":document.getElementById('order').value,
+             "entry.175951092":document.getElementById('times').options[document.getElementById('times').selectedIndex].value},
+      type:"POST",
+      dataType:"xml",
+      //statusCode: {0: afterSubmitHandler(payment, term), 200: afterSubmitHandler(payment, term)}
+    });
 }
